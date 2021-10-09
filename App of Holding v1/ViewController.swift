d713506c03a025d4b8a07acd7856301985a2c744
Richard Nava <richard.ig.nava@gmail.com>
2021-10-08 20:18:17 -0500

    Initial Commit

---
+++ App of Holding v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App of Holding v1
+//
+//  Created by Richard Centeno on 10/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
